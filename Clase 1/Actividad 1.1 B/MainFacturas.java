268a6f1bde1aa4c2f18eed6f5e1073892caa8292
E-Salman <esalman@hotmail.com>
2024-09-30 16:09:48 -0300

    Cambio nombre de clases

--- Clase 1/Actividad 1.1 B/MainFacturas.java
+++ Clase 1/Actividad 1.1 B/MainFacturas.java
@@ -1,22 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class MainFacturas {
     public static void main(String[] args) {
-        List<factura> facturas = new ArrayList<>();
-        facturas.add(new factura(1,2000,100));
-        facturas.add(new factura(2,1000,101));
-        facturas.add(new factura(3,1500,102));
-        facturas.add(new factura(4,2600,100));
+        List<Factura> facturas = new ArrayList<>();
+        facturas.add(new Factura(1,2000,100));
+        facturas.add(new Factura(2,1000,101));
+        facturas.add(new Factura(3,1500,102));
+        facturas.add(new Factura(4,2600,100));
 
         List<Cliente> clientes = new ArrayList<>();
         clientes.add(new Cliente(100,"Nico"));
         clientes.add(new Cliente(101,"Esteban"));
         clientes.add(new Cliente(102,"Zoe"));
-
+        
         List<SumaFacturas> resultado = sistemaFacturacion(facturas, clientes);
 
         for (int i = 0; resultado.size() > i; i++){
@@ -27,7 +26,7 @@ public class Main {
         }
     }
 
-    public static List sistemaFacturacion(List<factura> facturas, List<Cliente> clientes){
+    public static List sistemaFacturacion(List<Factura> facturas, List<Cliente> clientes){
         List<SumaFacturas> listaRetorno = new ArrayList<>();
         for(int i =0; i < clientes.size(); i++){
             listaRetorno.add(new SumaFacturas(clientes.get(i).IDcliente,clientes.get(i).nombre,0));
