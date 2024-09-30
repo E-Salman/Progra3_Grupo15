9e7a2cf2770a0eb87c6b5e8785434c1f477e27df
E-Salman <esalman@hotmail.com>
2024-09-30 19:34:22 -0300

    Agregado 1.1B con Maps y complejidad

--- Clase 1/Actividad 1.1 B/MainFacturas.java
+++ Clase 1/Actividad 1.1 B/MainFacturas.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -17,24 +18,40 @@ public class MainFacturas {
         clientes.add(new Cliente(102,"Zoe"));
         
         List<SumaFacturas> resultado = sistemaFacturacion(facturas, clientes);
+        System.out.println(sistemaFacturacionMaps(facturas, clientes).toString());
 
+        //Este for es para debuggeado, no lo tengo en cuenta en el calculo de complejidad
         for (int i = 0; resultado.size() > i; i++){
             System.out.println("Id cliente: "+ resultado.get(i).IDcliente);
-            System.out.println("nombre: " + resultado.get(i).nombre);
             System.out.println("importe total: " + resultado.get(i).importe);
             System.out.println();
         }
     }
 
+    //n * (n + n + n)
+    //Con Maps O(3n^2)
+    public static HashMap sistemaFacturacionMaps(List<Factura> facturas, List<Cliente> clientes){
+        HashMap<Integer, Integer> facturasClientes = new HashMap<>();
+        Factura factura;
+        for(int i = 0; i < facturas.size(); i++){
+            factura = facturas.get(i);
+            if(facturasClientes.containsKey(factura.IDcliente)) facturasClientes.put(factura.IDcliente, facturasClientes.get(factura.IDcliente) + factura.importe);
+            else facturasClientes.put(factura.IDcliente, factura.importe);
+        }
+        return facturasClientes;
+    }
+
+    //Sin Maps O(n + 2n^2 + 4n^3)
     public static List sistemaFacturacion(List<Factura> facturas, List<Cliente> clientes){
         List<SumaFacturas> listaRetorno = new ArrayList<>();
-        for(int i =0; i < clientes.size(); i++){
-            listaRetorno.add(new SumaFacturas(clientes.get(i).IDcliente,clientes.get(i).nombre,0));
+        for(int i = 0; i < clientes.size(); i++){ //n
+            listaRetorno.add(new SumaFacturas(clientes.get(i).IDcliente, 0)); //n
         }
-        for(int i = 0; i < facturas.size();i++){
-            for(int j = 0; j < listaRetorno.size(); j++){
-                if(facturas.get(i).IDcliente == listaRetorno.get(j).IDcliente){
-                    listaRetorno.get(j).importe += facturas.get(i).importe;
+
+        for(int i = 0; i < facturas.size();i++){ //n
+            for(int j = 0; j < listaRetorno.size(); j++){ //n
+                if(facturas.get(i).IDcliente == listaRetorno.get(j).IDcliente){ //2n
+                    listaRetorno.get(j).importe += facturas.get(i).importe; //2n
                     break;
                 }
             }
--- Clase 1/Actividad 1.1 B/SumaFacturas.java
+++ Clase 1/Actividad 1.1 B/SumaFacturas.java
@@ -1,12 +1,9 @@
 public class SumaFacturas {
     int IDcliente;
-    String nombre;
     int importe;
     
-    public SumaFacturas(int IDcliente, String nombre, int importe){
+    public SumaFacturas(int IDcliente, int importe){
         this.IDcliente = IDcliente;
-        this.nombre = nombre;
         this.importe = importe;
-
     }
 }
