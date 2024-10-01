1104a98a1653e99d800e41144215f6a3c1f12fbe
zoe <zoevarese26@gmail.com>
2024-10-01 20:37:01 -0300

    agregando actividad 1

--- Clase_4/Actividad_1/Main.java
+++ Clase_4/Actividad_1/Main.java
@@ -1,5 +1,28 @@
 package Clase_4.Actividad_1;
+
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] monedas = {10,5,15,25,1};
+        int vuelto = 23;
+
+        if(esExacto(monedas, vuelto)){
+            System.out.println("El vuelto es exacto");
+        }else{
+            System.out.println("El vuelto no es exacto");
+        }
+        
+    }
     
-    
+    public static boolean esExacto(int[] monedas, int vuelto){
+        Arrays.sort(monedas);
+        for (int i = monedas.length - 1; i >= 0; i--) {
+            while(vuelto >= monedas[i]){
+                vuelto -= monedas[i];
+            }
+        }
+        return vuelto==0;
+
+    }
 }
