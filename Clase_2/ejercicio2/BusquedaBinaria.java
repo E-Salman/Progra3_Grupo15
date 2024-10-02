51f823a30684a300d4902141a65af6f1f06da158
jjotaaa <julimaistruk015@gmail.com>
2024-10-01 22:14:23 -0500

    division de ejercicios Clase2

--- Clase_2/ejercicio2/BusquedaBinaria.java
+++ Clase_2/ejercicio2/BusquedaBinaria.java
@@ -1,6 +1,5 @@
-package Clase_2;
+package Clase_2.ejercicio2;
 public class BusquedaBinaria {
-
     // Método para realizar la búsqueda binaria
     public static int busquedaBinaria(int[] arreglo, int objetivo) {
         int izquierda = 0;
@@ -23,15 +22,13 @@ public class BusquedaBinaria {
                 derecha = medio - 1;
             }
         }
-
         // Si no se encuentra el objetivo, retorna -1
         return -1;
-    }
+       }
     public static void main(String[] args) {
         int[] arreglo = {1, 3, 5, 7, 9, 11, 13};
         int objetivo = 7;
         int resultado = busquedaBinaria(arreglo, objetivo);
-
         if (resultado == -1) {
             System.out.println("Elemento no encontrado en el arreglo.");
         } else {
