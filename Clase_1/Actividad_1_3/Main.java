f9aec951e770d729227e75f6be50ca8684a1f7c0
E-Salman <esalman@hotmail.com>
2024-10-03 15:16:23 -0300

    orden recursivo

--- Clase_1/Actividad_1_3/Main.java
+++ Clase_1/Actividad_1_3/Main.java
@@ -8,13 +8,17 @@ public class Main {
         System.out.println("Ingrese un numero para la suma recursiva: ");
         int numero = scanner.nextInt();
         System.out.println(sumaRecursiva(numero));
-    }
-
+    }    
+    
+    //SUSTRACCION
+    //a = 1
+    //b = 1
+    //k = 0
+    //ENTONCES --> O(n^(k + 1)) --> O(n)
     private static int sumaRecursiva(int i){
-        if(i==0){
+        if(i == 0){
             return 0;
         }
-        return i + sumaRecursiva(i-1);
+        return i + sumaRecursiva(i - 1);
     }
-
 }
